856ccd0881dc2abd2dfaf5edb4c25d072dc5417d
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-09 15:06:01 -0600

    Building a JAR File with Maven

--- JavaArchivesMaven/src/main/java/com/loonycorn/EntryPoint.java
+++ JavaArchivesMaven/src/main/java/com/loonycorn/EntryPoint.java
@@ -1,4 +1,29 @@
 package com.loonycorn;
 
 public class EntryPoint {
+
+    private String mode;
+
+    public EntryPoint(String mode) {
+        this.mode = mode;
+    }
+
+    public void printMode() {
+        System.out.println("App is running in " + this.mode + " mode.\n");
+    }
+
+    public static void main(String[] args) {
+
+        EntryPoint ep = new EntryPoint("DEV");
+
+        ep.printMode();
+
+        Employee admin = new Employee("admin", 0, "admin");
+
+        System.out.println("New admin user created from EntryPoint");
+
+    }
 }
+
+// Apache POM Reference
+// https://maven.apache.org/pom.html
