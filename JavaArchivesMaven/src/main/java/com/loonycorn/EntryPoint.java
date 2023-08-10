e32a4883ce1c780943981092f365cbbe40fb201d
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-09 21:08:06 -0600

    Building a JAR File with Dependencies

--- JavaArchivesMaven/src/main/java/com/loonycorn/EntryPoint.java
+++ JavaArchivesMaven/src/main/java/com/loonycorn/EntryPoint.java
@@ -1,15 +1,24 @@
 package com.loonycorn;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class EntryPoint {
 
     private String mode;
+    private static Logger logger;
 
     public EntryPoint(String mode) {
         this.mode = mode;
+
+        System.setProperty("log4j.configurationFile",
+                "/Users/juan/code/skillsoft/java-novice-to-javanista/track-2-java-apprentice/java-archive-jar-packaging-java-apps-using-maven/resources/log4j2.properties");
+        this.logger = LogManager.getLogger(EntryPoint.class);
+
     }
 
     public void printMode() {
-        System.out.println("App is running in " + this.mode + " mode.\n");
+        logger.info("App is running in " + this.mode + " mode.\n");
     }
 
     public static void main(String[] args) {
@@ -27,3 +36,6 @@ public class EntryPoint {
 
 // Apache POM Reference
 // https://maven.apache.org/pom.html
+
+// Invalidating the IntelliJ cache
+// File -> Invalidate Caches / Restart
